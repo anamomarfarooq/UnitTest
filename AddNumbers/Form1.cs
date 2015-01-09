a48c8b18fde513219c0da9761707929bec22a149
Anam Rafique <anam.omar.farooq@gmail.com>
2015-01-09 18:09:24 +0800

    Unit test Sample project

---
+++ AddNumbers/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int result;
+            result = Add(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            MessageBox.Show("Result" + result);
+        }
+
+        public int Add(int x, int y)
+        {
+            int z;
+            z = x + y;
+            return z;
+        }
+    }
+}
---
+++ AddNumbers_Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddNumbers;
+using System.Windows.Forms;
+
+namespace AddNumbers_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Add_Testing()
+        {
+            //Arrange
+            int x = 1;
+            int y = 1;
+            var number = new Form1();
+            //Act
+            int actual = number.Add(x, y);
+
+            //Assert
+            int expected = 2;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
